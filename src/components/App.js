e87b29c88ebd044c6731032a3b9c3d948d5d84c2
sk sohel rahaman <sohelrahaman43@gmail.com>
2023-05-10 15:13:34 +0530

    submitting code-Wed May 10 2023 15:13:31 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,26 @@
 
 import React from "react";
-import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-let  [msg, useMsg]=useState("")
-console.log(msg)
-
+  function greeting(event) {
+    event.preventDefault();
+    let greet = event.target.value;
+    if(greet != "") {
+      document.getElementById('greeting').innerHTML = `Hello ${greet}!`;
+    } else {
+      document.getElementById('greeting').innerHTML = "";
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
-      <form >
-        
-        <label htmlFor="name">Enter your name :</label>
-        <br />
-        <input id="name" type="text" onChange={(event)=>{useMsg(event.target.value)}}/>
-      </form>
-      <p id="greet">
-        {
-          msg ? `hello ${msg}!` : ""
-          
-        }
-      </p>
+        <form>
+          <label for="name">Enter your name:</label>
+          <br />
+          <input id="name" type="text" onInput={greeting} />
+        </form>
+        <p id="greeting"></p>
     </div>
   )
 }
