c659531ea0156fd04076fd37347e204fd49fe0fa
sk sohel rahaman <sohelrahaman43@gmail.com>
2023-05-10 14:57:54 +0530

    submitting code-Wed May 10 2023 14:57:41 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+let  [msg, useMsg]=useState("")
+console.log(msg)
+
   return (
     <div>
         {/* Do not remove the main div */}
+      <form >
+        
+        <label htmlFor="name">Enter your name :</label>
+        <br />
+        <input id="name" type="text" onChange={(event)=>{useMsg(event.target.value)}}/>
+      </form>
+      <p id="greet">
+        {
+          msg ? `hello ${msg}!` : ""
+          
+        }
+      </p>
     </div>
   )
 }
